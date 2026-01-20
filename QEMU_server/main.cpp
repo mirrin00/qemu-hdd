157a294d222b925e47f31f2e9147ddeea970822f
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2026-01-20 20:36:17 +0300

    [ADD] Create server

---
+++ QEMU_server/main.cpp
@@ -0,0 +1,43 @@
+#include <arpa/inet.h>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+
+int main() {
+    sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(31234),
+        .sin_zero = 0
+    };
+
+    inet_aton("127.0.0.1", &addr.sin_addr);
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+
+    listen(fd, 1);
+    int client_fd = accept(fd, nullptr, nullptr);
+
+    char buffer[1024] = {};
+    while (true) {
+        ssize_t bytes = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes < 0) {
+            break;
+        }
+
+        std::string str(static_cast<size_t>(bytes), '\0');
+        std::memcpy(str.data(), buffer, bytes);
+
+        if (str.empty()) {
+            break;
+        }
+
+        for (const char ch: str) {
+            std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(ch) << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
